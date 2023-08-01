ba863c5491bc5a382078d1e129cae74e8b327a21
Scott <scott@scottlew.ca>
2023-08-01 13:41:42 -0400

    Adding basic github structure for actions

---
+++ main.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    github = {
+      source  = "integrations/github"
+      version = "~> 5.0"
+    }
+  }
+}
+
+# Configure the GitHub Provider
+provider "github" {
+    token = var.GITHUB_TOKEN
+}
+
+data "github_repository" "test" {
+  full_name = "scott-lew/test"
+}
+
+output "test" {
+  value = data.github_repository.test
+}
+
+variable "GITHUB_TOKEN" {}
